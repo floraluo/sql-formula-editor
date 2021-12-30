7893cba507f91687f7ca95838479d7a7c2f7ea8a
wb_luofang <wb_luofang@meituan.com>
2021-12-30 19:34:23 +0800

    dd

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
 </template>
-
+ 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
 import HelloWorld from './components/HelloWorld.vue';
