4f790e4c7bda9c2053fae993b6623612cdf3843d
floraluo <mrsluofang@qq.com>
2024-09-11 21:47:05 +0800

    feat: sql公式

--- README.md
+++ README.md
@@ -1,19 +1,37 @@
 # sql-formula-editor
 
-## Project setup
-```
+## 项目说明
+
+公式编辑器
+  说明 | 路径
+  --- | ---
+  按钮配置| `/src/lib/editorButtonsMap.js`
+  组件 | `/src/components/common/editor`
+  组件说明 | `/src/components/common/editor/editor.md`
+  公式格式化|`/src/utils/formatFormula.js`
+  公式解析|`/src/utils/parseFormula.js`
+  公式节点渲染说明|`/src/components/common/editor/renderNodeModel.md`
+  组件弹窗说明|`/src/components/common/ModalAddField.md`
+## 项目搭建
+
+```bash
 yarn install
-```
 
 ### Compiles and hot-reloads for development
-```
 yarn serve
-```
 
 ### Compiles and minifies for production
-```
 yarn build
 ```
 
 ### Customize configuration
+
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+## 项目依赖
+
+### dev工具
+
+### 第三方插件
+
+- [element-ui](https://element.eleme.cn/#/zh-CN/component/tooltip)
--- src/components/editor/Node.js
+++ src/components/editor/Node.js
@@ -1,24 +1,91 @@
-function Node(opt) {
-  // Object.keys(opt).forEach(key => {
-  //   this[key] = opt[key];
+import { nodeChildrenMap } from './nodeMaps'
+
+function Node({ opt, parentNode }) {
+//   place
+  // let nodeIndex;
+  // let layerIndex;
+  // console.log('parentNode :>> ', parentNode);
+  this.parentNode = parentNode;
+
+  this.seat = {
+    nodeIndex: undefined,
+    layerIndex: undefined
+  }
+  // const watchedProperty = ['nodeIndex', 'layerIndex'];
+  // watchedProperty.forEach(property => {
+  //   Object.defineProperty(this, property, {
+  //     get() {
+  //       return this.seat[property]
+  //     },
+  //     set(newValue) {
+  //       this.seat[property] = newValue;
+  //       if (this.parentNode.name === 'root') {
+  //         this.path = `${this.nodeIndex}`;
+  //       } else {
+  //         this.path = `${this.parentNode.path}-${this.layerIndex}-${this.nodeIndex}`;
+  //       }
+  //       this.updateChildPath();
+  //     },
+  //     enumerable: true,
+  //     configurable: true
+  //   })
   // })
-  this.place = opt.place;
-  this.parentNode = opt.parentNode;
-  this.levelIndex = opt.levelIndex;
-  this.nodeIndex = opt.nodeIndex;
+  Object.keys(opt).forEach(key => {
+    this[key] = opt[key];
+  })
+  if (!opt.children) {
+    const getNodeChildren = nodeChildrenMap[opt.renderType];
+    this.children = getNodeChildren ? getNodeChildren(this): undefined;
+  }
+  Object.defineProperty(this, 'path', {
+    get() {
+      if (this.parentNode) {
+        const parentNodePath = this.parentNode && this.parentNode.path;
+        const pathArr = [];
+        parentNodePath !== undefined && pathArr.push(parentNodePath);
+        this.layerIndex !== undefined && pathArr.push(this.layerIndex);
+        pathArr.push(this.nodeIndex);
+        return pathArr.join('-');
+      } else {
+        return undefined;
+      }
+
+    },
+    set() { 
+      console.log('set Node Path :>> ', this.path);
+    }
+  })
 }
+
 Node.prototype = {
   constructor: Node,
-  insertChildNode() { },
+  moveBack() { 
+    this.nodeIndex--;
+  },
+  moveForward() { 
+    this.nodeIndex++;
+  },
+  updateChildPath() { 
+    if (this.children && this.children.length) {
+      this.children.forEach((childNodeList, layerIndex) => {
+        childNodeList.forEach((child, index) => {
+          // child.
+        })
+      })
+    }
+  },
+
+  initNode() { 
+
+  },
+  insertNode() { 
+    
+  },
   deleteChildNode() { },
   getPath() {
     return `${this.parentNode.path}-${this.levelIndex}-${this.nodeIndex}`
   },
-  _stepBack() {
-    this.nodeIndex--;
-  },
-  _stepForward() { 
-    this.nodeIndex++;
-  },
+  
 
 }
+export default Node;
